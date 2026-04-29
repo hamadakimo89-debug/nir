2630c1c5233f64067576aec64fc32a37556b2d8d
hamadakimo89-debug <hamadakimo89@gmail.com>
2026-04-29 08:59:31 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,50 @@
-const cacheName = 'expenses-v1';
-const assets = ['index.html', 'manifest.json'];
-
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(cacheName).then(c => c.addAll(assets)));
+const CACHE_NAME = 'masroufat-noor-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon.png',
+  './icon-512.png'
+];
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+  self.skipWaiting();
 });
-
-self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+      return fetch(event.request).then((networkResponse) => {
+        if (networkResponse && networkResponse.status === 200) {
+          const responseClone = networkResponse.clone();
+          caches.open(CACHE_NAME).then((cache) => {
+            cache.put(event.request, responseClone);
+          });
+        }
+        return networkResponse;
+      }).catch(() => {
+        return caches.match('./index.html');
+      });
+    })
+  );
 });
